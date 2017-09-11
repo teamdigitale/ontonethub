dc7f96ae899c97a192bb804476fda4719bfa3eaa
anuzzolese <andrea.nuzzolese@gmail.com>
2017-09-11 18:15:05 +0200

    Fixed docker description.

--- src/startup.sh
+++ src/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ ! -f /usr/local/tomcat/webapps/stanbol.war ]; then
-    cp war/target/stanbol.war /usr/local/tomcat/webapps
-    echo 'The file stanbol.war has been moved to the webapps folder.'
-else echo 'The file stanbol.war already exists into the webapps folder.'
-fi
+#if [ ! -f /usr/local/tomcat/webapps/stanbol.war ]; then
+#    cp war/target/stanbol.war /usr/local/tomcat/webapps
+#    echo 'The file stanbol.war has been moved to the webapps folder.'
+#else echo 'The file stanbol.war already exists into the webapps folder.'
+#fi
 
 while :
 do
